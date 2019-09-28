b6b46f8edab30d1a1d30fa7cdab5521d4e3a151f
alxkorn <akorneyev10@yandex.ru>
2019-09-28 16:19:00 +0300

    Added module for constants

---
+++ bj_constants.rb
@@ -0,0 +1,18 @@
+module BJConstants
+  
+  def const_high_rnak_value
+    10
+  end
+
+  def const_ace_value_high
+    11
+  end
+
+  def const_ace_value_low
+    1
+  end
+
+  def const_black_jack
+    21
+  end
+end
